8c514148f707019b8d70252999151f880ce6ba8c
Jonas HAOUZI <haouzijonas@gmail.com>
2023-02-08 00:33:46 +0100

    u

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import { Router } from "@fastly/expressly";
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
-const PRODUCTS_BACKEND = "origin_0";
-const router = new Router();
-let backendResponse;
-
-// If the URL begins with /products/
-router.all("(.*)", async (req, res) => {
-  // Parse the JSON response from the backend.
-  const data = await backendResponse.json();
-
-  // Add a new field to the parsed data.
-  data["new_field"] = "data injected at the edge";
-
-  // Construct a new response using the new data but original status and headers.
-  res.headers = backendResponse.headers;
-  res.withStatus(backendResponse.status).json(data);
-});
-
-router.listen();
+async function handleRequest(event) {
+  return new Response("OK", { status: 200 });
+}
