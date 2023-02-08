f1003853089553a313ac5ecc8d2a6f5e132d836a
Jonas HAOUZI <haouzijonas@gmail.com>
2023-02-08 14:06:16 +0100

    f

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
-/// <reference types="@fastly/js-compute" />
- 
-const handler = async (event) => {
-  // Get the request from the client.
-  const req = event.request
-console.log(req);  
-const beresp = await fetch(req, {
-    backend: "origin_0"
-  })
+import { Router } from "@fastly/expressly";
 
+const PRODUCTS_BACKEND = "origin_0";
+const router = new Router();
+let backendResponse;
 
-  // Send our response back to the client.
-  return beresp;
-};
+// Configure middleware that automatically proxies request
+// asynchronously to your backend and sets the global variable
+// for use in all your base request handlers.
+router.use(async (req, res) => {
+  backendResponse = await fetch(req, {
+    backend: PRODUCTS_BACKEND
+  });
+});
 
-addEventListener("fetch", event => event.respondWith(handler(event)));
+// If the URL begins with /products/
+router.all("(.*)", async (req, res) => {
+  // Parse the JSON response from the backend.
+  const data = await backendResponse.text();
+  console.log("JOJO");
+  console.log(typeof data);
+  console.log(data);
+  let data2 = data.replace('ok', 'ok-but-optimized.jpg');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('a', 'b');
+  data2 = data2.replace('ok', 'ok-but-optimized.jpg');
+  data2 = data2.replace('ok', 'ok-but-optimized.jpg');
+console.log(typeof res);
+console.log(res.constructor.name);
+  console.log(data2);
+
+  // Construct a new response using the new data but original status and headers.
+  //res.headers = backendResponse.headers;
+  res.withStatus(backendResponse.status).send(data2);
+});
+
+router.listen();
