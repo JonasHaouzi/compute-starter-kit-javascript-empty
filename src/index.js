f4e84ae66e19f4940b78d0c452ada91e717d7e55
Jonas HAOUZI <haouzijonas@gmail.com>
2023-02-08 11:12:17 +0100

    y

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
-/// <reference types="@fastly/js-compute" />
+addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
-addEventListener("fetch", async (event) => {
+async function handleRequest(event) {
+  return new Response("OK", { status: 200 });
+}
 
-  const req = event.request;
-
-  let backendResponse = await fetch(req, {
-    backend: "origin_0"
-  });
-
-/*  if (backendResponse.status == 200) {
-    // Parse the URL so we can determine the path.
-    let url = new URL(req.url);
-console.log("JOJO");
-    // If the request is to a product endpoint...
-    if (url.pathname.startsWith('/products/')) {
-      // Parse the JSON response from the backend.
-      let data = await backendResponse.json();
-
-      // Add a new field to the parsed data.
-      data["new_field"] = "data injected at the edge";
-
-      // Construct a new response using the new data but original status and headers.
-      backendResponse = new Response(JSON.stringify(data), {
-        status: backendResponse.status,
-        headers: backendResponse.headers
-      });
-    }
-  }
-*/
-  // Send the backend response to the client.
-  event.respondWith(backendResponse);
-});
